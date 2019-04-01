ea5ab79b9d05df46f7dd1e91b144aacfbd3ee090
Paul-Owori <prowori2@gmail.com>
2019-04-01 15:28:47 +0300

    Add alerts to newCM.js and newSP.js for testing functionality of signup

--- js/newCM.js
+++ js/newCM.js
@@ -8,14 +8,18 @@ var cmPassword = $("#pwd").val();
 var cmUserName = $("#username").val();
 
 
-function sp (cmUserName, cmName, cmPassword, cmEmail, floorNmbr, officeName){
-  this.userName = cmUserName;
-  this.cmName = cmName;
-  this.cmPassword = cmPassword;
-  this.cmEmail = cmEmail;
-  this.officeName = officeName;
-  this.floorNmbr = floorNmbr;
+function cm (cUserName, cName, cPassword, cEmail, cfloor, oName){
+  this.userName = cUserName;
+  this.cmName = cName;
+  this.cmPassword = cPassword;
+  this.cmEmail = cEmail;
+  this.officeName = oName;
+  this.floorNmbr = cfloor;
   this.ID = Math.floor((Math.random() * 200000000) + 100000000);
   customerArray.push([this.ID, this.userName, [this]]);
   //pushes the new contact contained within its own mini array to the customerArray in the format [this.ID,this.userName,[this]]
 };
+
+var customer = new cm (cmUserName, cmName, cmPassword, cmEmail, floorNmbr, officeName);
+
+alert(customerArray)
