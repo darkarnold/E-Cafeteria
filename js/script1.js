e1269ddb899b1f828fb9e4f48997ef7b5517d679
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-04-02 08:54:19 +0300

    remove some css properties

--- js/script1.js
+++ js/script1.js
@@ -1,22 +1,27 @@
-// Get the id of the sign up  button on the overlay container
-const signUpButton = document.getElementById('Signup');
-// Get the id of the ghost login button on the overlay container
-const loginButton = document.getElementById("Login");
-//get the id of the container
-const container = document.getElementById("container");
+$(document).ready(function(event){
+  // Get the id of the sign up  button on the overlay container
+  const signUpButton = document.getElementById('Signup');
+  // Get the id of the ghost login button on the overlay container
+  const loginButton = document.getElementById("Login");
+  //get the id of the container
+  const container = document.getElementById("container");
 
-// Event listeners to switch panels when clicked
+  // Event listeners to switch panels when clicked
+  $("signUpButton").click(function(event) {
+    // Prevent the page from reloading
+    event.preventDefault();
+    container.classList.add("panel-active");
+  });
 
-$("signUpButton").click(function() {
-  container.classList.add("panel-active");
-    container.classList.remove("panel-active");
+  $("loginButton").click()
+
+  /*signUpButton.addEventListener("click",() =>{
+
+  });
+
+  signUpButton.addEventListener("click",() =>{
+
+  });
+  */
+
 });
-
-/*signUpButton.addEventListener("click",() =>{
-
-});
-
-signUpButton.addEventListener("click",() =>{
-
-});
-*/
