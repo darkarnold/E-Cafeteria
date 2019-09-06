cd64039975372f442f3da5dc21aa6adca69e581c
Paul-Owori <prowori2@gmail.com>
2019-09-06 14:22:51 +0300

    Removed node_modules, prepared index.js for deployment to heroku

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-var express = require('express');
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
 
-var app = express();
+//Set static folder
 app.use(express.static('public'));
 
+//Start app from homepage
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/home.html'));
+});
 
-var port = process.env.PORT || 3000;
-app.listen(port);
-
-//app.set('port', process.env.PORT || 3000);
-//app.listen(3000)
-
-app.get('/home', function(req, rep){
+//Direct any requests for /home to homepage
+app.get('/home', (req, rep) => {
   rep.sendFile(__dirname + '/public/home.html');
-})
+});
+
+app.listen(port, () => {
+  console.log("E-Cafeteria is running")
+});
